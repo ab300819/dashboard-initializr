0a89ea17bfea88b680cfafd19be38e4d2bddd862
Mason <ab300819@163.com>
2019-03-13 00:57:17 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HTMLPlugin=require('html-webpack-plugin');
 const webpack=require('webpack');
 
