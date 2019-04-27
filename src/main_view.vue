0c7eb352227db8bf7d12149c00576224df10d693
Mason <ab300819@163.com>
2019-04-27 18:36:59 +0800

    实现简单聊天窗口

--- src/main_view.vue
+++ src/main_view.vue
@@ -1,19 +1,33 @@
 <template>
-    <el-container>
-        <el-header>Header</el-header>
+    <div class="main_container">
         <el-container>
-            <el-aside width="200px">Aside</el-aside>
+            <el-header>Chat Online</el-header>
             <el-container>
-                <el-main>Main</el-main>
-                <el-footer>Footer</el-footer>
+                <el-aside ><chat_list></chat_list></el-aside>
+                <el-container>
+                    <el-main>
+                        <chat_window></chat_window>
+                    </el-main>
+                    <el-footer><chat_input></chat_input></el-footer>
+                </el-container>
             </el-container>
         </el-container>
-    </el-container>
+    </div>
+
 </template>
 
 <script>
+    import chat_window from "./chat_window.vue"
+    import chat_input from  "./chat_input.vue"
+    import chat_list from "./chat_list.vue"
+
     export default {
-        name: "main_view"
+        name: "main_view",
+        components: {
+            chat_window,
+            chat_input,
+            chat_list
+        }
     }
 </script>
 
@@ -50,5 +64,9 @@
 
     .el-container:nth-child(7) .el-aside {
         line-height: 320px;
+    }
+
+    .main_container {
+        width: 85%;
     }
 </style>
